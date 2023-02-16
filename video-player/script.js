a6fce66e5e4c1249f1b40cc7b630730e03d7cd1b
SnakeEyes0723 <jawie0723@gmail.com>
2023-02-15 21:23:27 -0600

    -excercise: fixed propagation problem

--- video-player/script.js
+++ video-player/script.js
@@ -3,5 +3,8 @@ const box = document.querySelector('div');
 const video = document.querySelector('video');
 
 btn.addEventListener('click', () => box.classList.remove('hidden'));
-video.addEventListener('click', () => video.play());
+video.addEventListener('click', (event) => {
+  event.stopPropagation();
+  video.play();
+});
 box.addEventListener('click', () => box.classList.add('hidden'));
